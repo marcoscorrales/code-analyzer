c8d2b0e43fdea468bd6d7e756673bea4f1d9db27
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-03-19 21:02:30 +0100

    Cambio favicon y cambio estilos de la pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,24 +16,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-black h-screen w-screen">
-        <h1 className="text-3xl text-white font-bold bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 text-transparent bg-clip-text">
+      <main className="bg-black h-screen w-screen flex flex-col items-center">
+        <h1 className="text-3xl text-center text-white font-bold bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 text-transparent bg-clip-text">
           Code analyzer
         </h1>
-        <form className="mt-8">
+        <form className="mt-8 w-full max-w-[1200px] h-[300px]">
         <label
           for="UserEmail"
-          class="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 dark:border-gray-700 dark:bg-gray-800"
+          class="block h-full overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 dark:border-gray-700 dark:bg-gray-800"
         >
           <span class="text-xl font-medium text-gray-700 dark:text-gray-200">
             Código
           </span>
 
-          <input
-            type="email"
-            id="UserEmail"
+          <textarea
+            type="text"
+            id="Code"
             placeholder="Escribe aquí tu código"
-            class="mt-1 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 dark:text-white sm:text-sm"
+            class="mt-1 w-full h-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 dark:text-white sm:text-sm" 
           />
         </label>
         </form>
